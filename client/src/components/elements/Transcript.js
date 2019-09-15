7519fd5e32030194f62c537f473c68d28655c681
Raman Mangla <ramanmangla7@gmail.com>
2019-09-15 08:52:57 -0400

    Bug fixes

--- client/src/components/elements/Transcript.js
+++ client/src/components/elements/Transcript.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Dialogue from "./Dialogue";
+import Currency from "./CurrencyAPI";
 
 const Transcript = () => {
+  const [showDialog, setDialog] = useState(false);
+
+  const onClick = () => {
+    setDialog(!showDialog);
+  };
+
   return (
     <div className='transcript-box'>
+      <button class='button-xe' onClick={onClick}>
+        Xe
+      </button>
+      {showDialog && <Currency />}
       <Dialogue message="Daman: Hey guys! What's up?" />
       <Dialogue message="Dev: I'm all good! How's everyone?" />
     </div>
