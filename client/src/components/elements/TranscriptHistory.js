d176067a46fe95a2979a80e480e01d0c55d938bb
Raman Mangla <ramanmangla7@gmail.com>
2019-09-14 21:09:28 -0400

    Added History page components and styling

---
+++ client/src/components/elements/TranscriptHistory.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TranscriptHistory = () => {
+  return (
+    <div className='transcript-box'>
+      <ul class='transcript-history'>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+        <li>Lorem Ipsum</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TranscriptHistory;
